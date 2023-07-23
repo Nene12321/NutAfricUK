fbfefe402cb73cf3f5ed0f0c6de333323d5049d5
Nene <w1929123@my.westminster.ac.uk>
2023-07-23 19:47:30 +0100

    Initial Commit

---
+++ NutAfriUK/NutAfriUKApp.swift
@@ -0,0 +1,17 @@
+//
+//  NutAfriUKApp.swift
+//  NutAfriUK
+//
+//  Created by Nene ogba on 23/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct NutAfriUKApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
